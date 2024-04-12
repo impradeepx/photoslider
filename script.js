1b75a336a12f60a50ae4ff389fd0203824b8c43a
Pråðëëp <im.pradeepx@gmail.com>
2024-04-12 14:32:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slider img');
+
+function showSlide(n) {
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex].style.display = 'block';
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+function prevSlide() {
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+// Show the first slide initially
+showSlide(slideIndex);
